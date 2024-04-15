0df5a5ba853c972a4c4ccc4eb45bcf06e8fc1863
devops-ds8 <samsamsam@gmail.com>
2024-04-15 17:21:28 +0100

    formatting

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,11 @@
-const { PORT } = require('../index'); // Import the app and valRef1
+const { PORT } = require("../index"); // Import the app and valRef1
 
-describe('Server', () => {
-  it('valRef1 has the correct value', () => {
+describe("Server", () => {
+  it("valRef1 has the correct value", () => {
     expect(PORT).toBe(3001);
   });
 });
 
-test('simple test', () => {
-    expect(1 + 1).toBe(2);
-  });
-
+test("simple test", () => {
+  expect(1 + 1).toBe(2);
+});
